0d5e1d19c07c8e808ac15ad389b6dff7a985e5b5
Bernd Beuster <bernd.beuster@googlemail.com>
2014-04-05 20:47:51 +0200

    Start USB enumbering.

--- sim/tb_top.sv
+++ sim/tb_top.sv
@@ -2,7 +2,11 @@ module tb_top;
    timeunit 1ns;
    timeprecision 1ps;
 
-   const realtime tclk24=1s/24e6;
+   import types::*;
+
+   const realtime tclk24 = 1s/24e6,
+                  tusb   = 1s/1.5e6;  // low speed
+   const int      nbit   = tusb/tclk24;
 
    bit  [1:0]   CLOCK_24;                               //      24 MHz
    bit  [1:0]   CLOCK_27;                               //      27 MHz
@@ -84,23 +88,139 @@ module tb_top;
    wire [35:0]  GPIO_0;                                 //      GPIO Connection 0
    wire [35:0]  GPIO_1;                                 //      GPIO Connection 1
 
+   logic        reset;     // reset
+   logic        clk;       // system clock (24 MHz)
+   var d_port_t d_i;       // USB port D+,D- (input)
+   var d_port_t tx_d_o;       // USB port D+,D- (output)
+   wire         tx_d_en;      // USB port D+,D- (enable)
+   bit   [7:0]  tx_data;   // data from SIE
+   bit          tx_valid;  // rise:SYNC,1:send data,fall:EOP
+   wire         tx_ready; // data has been sent
+
+   byte GET_DESCRIPTOR[]='{8'h80,8'h06,8'h00,8'h01,8'h00,8'h00,8'h08,8'h00};
+   byte SHORT_DEVICE_DESCRIPTOR[]='{8'd18,8'h01,8'h10,8'h01,8'h00,8'h00,8'h00,8'h08};
+   byte DEVICE_DESCRIPTOR[]='{8'd18,8'h01,8'h10,8'h01,8'h00,8'h00,8'h00,8'h08,
+			      8'hd8,8'h04,8'h01,8'h00,8'h00,8'h02,8'h01,8'h02,
+			      8'h00,8'h01};
    CII_Starter_TOP dut(.*);
 
-   initial forever #(tclk24/2) CLOCK_24=~CLOCK_24;
+   usb_tx tb_tx(.reset(reset),
+		.clk(clk),
+		.d_o(tx_d_o),
+		.d_en(tx_d_en),
+		.data(tx_data),
+		.valid(tx_valid),
+		.ready(tx_ready));
+
+
+   initial forever #(tclk24/2) CLOCK_24 = ~CLOCK_24;
+
+   always_comb reset = ~KEY[0];
+   always_comb clk = CLOCK_24;
+
+   assign {GPIO_1[34],GPIO_1[32]} = (tx_d_en) ? tx_d_o : 2'bz;
 
    initial
-     begin
+     begin:main
+	$timeformat(-9,3," ns");
+
 	/* reset */
 	KEY[0]=1'b0;
         #100ns KEY[0]=1'b1;
 
+	/**********************************************************************
+	 * Control Read Transfer
+	 **********************************************************************/
 
-	#3us SW[1]=1'b1;
-	#3us SW[1]=1'b0; SW[2]=1'b1;
-	#3us SW[2]=1'b0; SW[3]=1'b1;
-	#3us SW[3]=1'b0; SW[4]=1'b1;
-	#3us SW[4]=1'b0;
+	/* Setup Transaction */
+	send_token(SETUP,0,0);
+	send_data(DATA0,GET_DESCRIPTOR);
+	//receive_pid(ACK);
 
-        #3us $stop;
-     end
+
+        #1ms $stop;
+     end:main
+
+
+   task send_token(input pid_t pid,input [6:0] addr,input [3:0] endp);
+      /* PID */
+      @(posedge clk);
+      tx_valid <= 1'b1;
+      tx_data  <= {~pid,pid};
+      do @(posedge clk); while (!tx_ready);
+
+      /* ADDR and first bit of ENDP */
+      tx_data = {endp[0],addr};
+      do @(posedge clk); while (!tx_ready);
+
+      /* Rest of ENDP and CRC5 */
+      tx_data = {crc5({endp,addr}),endp[3:1]};
+      do @(posedge clk); while (!tx_ready);
+
+      /* wait for last byte */
+      do @(posedge clk); while (!tx_ready);
+      tx_valid = 1'b0;
+
+      /* wait for EOP */
+      do @(posedge clk); while (tx_d_en);
+   endtask
+
+   task send_data(input pid_t pid,input byte data[]='{});
+      /* PID */
+      @(posedge clk);
+      tx_valid <= 1'b1;
+      tx_data  <= {~pid,pid};
+      do @(posedge clk); while (!tx_ready);
+
+      foreach (data[i])
+	begin
+	   tx_data = data[i];
+	   do @(posedge clk); while (!tx_ready);
+	end
+
+      /* CRC16 */
+      for(int i=0;i<16;i+=8)
+	begin
+	   tx_data = crc16(data)[7+i-:8];
+	   do @(posedge clk); while (!tx_ready);
+	end
+
+      /* wait for last byte */
+      do @(posedge clk); while (!tx_ready);
+      tx_valid = 1'b0;
+
+      /* wait for EOP */
+      do @(posedge clk); while (tx_d_en);
+   endtask
+
+   function [4:0] crc5(input [10:0] d);
+      const bit [4:0] crc5_poly=5'b10100,
+		      crc5_res =5'b00110;
+
+      crc5='1;
+
+      for(int i=$right(d);i<=$left(d);i++)
+	if(crc5[$right(crc5)]^d[i])
+	  crc5=(crc5>>1)^crc5_poly;
+	else
+	  crc5=crc5>>1;
+
+      crc5=~crc5;
+   endfunction
+
+   function [15:0] crc16(input byte d[]='{});
+      const bit [15:0] crc16_poly=16'b1010000000000001,
+		       crc16_res =16'b1011000000000001;
+
+      crc16='1;
+
+      foreach(d[j])
+	for(int i=$right(d[j]);i<=$left(d[j]);i++)
+	  if(crc16[$right(crc16)]^d[j][i])
+	    crc16=(crc16>>1)^crc16_poly;
+	  else
+	    crc16=crc16>>1;
+
+      crc16=~crc16;
+   endfunction
 endmodule
