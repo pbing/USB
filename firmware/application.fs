e7b486ca7a3f72b581c53aa0def7650209a19e46
Bernd Beuster <bernd.beuster@googlemail.com>
2014-04-05 18:25:58 +0200

    Insert device-response.

--- firmware/application.fs
+++ firmware/application.fs
@@ -2,18 +2,7 @@
 module[ application"
 
 include device.fs
-include hex-display.fs
-include led.fs
 
-: wait      ( u -- ) 0do loop ;
-: wait-1000 ( u -- ) 0do d# 1000 wait loop ;
-
-: main ( --)
-  d# 1
-  begin
-    hex-select
-    led-walk
-    d# 100 wait-1000
-  again ;
+: main ( --)   begin  device-response  again ;
 
 ]module
