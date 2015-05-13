a5e9da4b72882cb28b67bc716c6942e2d0abfe28
Bernd Beuster <bernd.beuster@gmail.com>
2015-05-13 16:42:45 +0200

    Formatting.

--- sim/tb_usb_cdr.sv
+++ sim/tb_usb_cdr.sv
@@ -4,10 +4,10 @@ module tb_usb_cdr;
 
    import types::*;
 
-   const realtime tclk=1s/24.0e6,
-		  tbit=1s/1.5e6;
+   const realtime tclk = 1s/24.0e6,
+		  tbit = 1s/1.5e6;
 
-   bit          reset=1'b1;
+   bit          reset = 1'b1;
    bit          clk;
    var d_port_t d;
    d_port_t     q;
@@ -16,25 +16,25 @@ module tb_usb_cdr;
 
    usb_cdr dut(.*);
 
-   initial forever #(tclk/2) clk=~clk;
+   always #(tclk/2) clk = ~clk;
 
    initial
      begin
-	reset<=#(2*tclk) 1'b0;
-	d=J;
+	reset <= #(2*tclk) 1'b0;
+	d = J;
 
 	if($test$plusargs("neg_phase"))
-	  #0.94us $display("phase<0");
+	  #0.94us $display("phase < 0");
 	else if($test$plusargs("pos_phase"))
-	  #1.60us $display("phase>0");
+	  #1.60us $display("phase > 0");
 	else
-	  #1.23us $display("phase=0");
+	  #1.23us $display("phase = 0");
 
 	/* SYNC */
-	repeat(7) #tbit d=(d==J)?K:J;
+	repeat(7) #tbit d = (d == J) ? K : J;
 	#(2*tbit);
 
-	repeat(64) #tbit d=({$random}%2)?K:J;
+	repeat(64) #tbit d = ({$random}%2) ? K : J;
 
 	#100ns $stop;
      end
